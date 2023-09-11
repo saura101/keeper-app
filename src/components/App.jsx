d6e72487f1bdfdd9238fd28930d56fc020d60f0b
Souradeep <souradeepghosh00@gmail.com>
2023-09-11 12:58:57 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note"
+import notes from "../notes";
+
+
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            {notes.map((note)=> {
+                return (
+                    <Note 
+                    title={note.title} 
+                    con={note.content} 
+                    key={note.key}/> 
+                );
+            })}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
